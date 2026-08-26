c30a935a5513c5e6f26846e64d564fbf68668759
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-08-26 14:49:56 +0530

    add tools section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import { assets, infoList } from '@/asset/assets'
+import { assets, infoList, toolsData } from '@/asset/assets'
 
 const About = () => {
 	return (
@@ -22,11 +22,21 @@ const About = () => {
 
 					<ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
 						{infoList.map(({ icon, iconDark, title, description }, index) => (
-							<li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer'
+							<li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black'
 								key={index}>
 								<Image src={icon} alt={title} className='w-7 mt-3'/>
 								<h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
 								<p className='text-gray-600 text-sm'>{description}</p>
+							</li>
+						))}
+					</ul>
+
+					<h4 className='my-6 text-gray-700 font-ovo'>Tools I use</h4>
+
+					<ul className='flex items-center gap-3 sm:gap-5'>
+						{toolsData.map((tool,index)=> (
+							<li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500 ' key={index}>
+								<Image src={tool} alt='Tool' className='w-5 sm:w-7'/>
 							</li>
 						))}
 					</ul>
