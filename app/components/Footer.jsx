b6c7520af2374cfba6bc6a19d3709dd36fcf86bb
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-08-26 14:51:41 +0530

    add Footer component

---
+++ app/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '@/asset/assets'
+
+const Footer = () => {
+  return (
+	<div className='mt-20'>
+	  <div className='text-center'>
+		<Image src={assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+
+		<div className='w-max flex items-center gap-2 mx-auto'>
+			<Image src={assets.mail_icon} alt='' className='w-6'/>
+				sandhyasingh950823256@gmail.com
+		</div>
+	  </div>
+
+		<div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+			<p>© 2026 Sandhya Kumari. All rights reserved.</p>
+			<ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+				<li><a target='_blank' href="https://github.com/heyasandhya">GitHub</a></li>
+				<li><a target='_blank' href="https://www.linkedin.com/in/sandhyakumari2/">Linkedin</a></li>
+			</ul>
+		</div>
+
+	</div>
+  )
+}
+
+export default Footer
