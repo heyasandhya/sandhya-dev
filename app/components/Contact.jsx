3fd076abc6ce47a0e6eff9248d9c7d5d5a612271
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-08-26 14:51:22 +0530

    add Contact component

---
+++ app/components/Contact.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { assets } from '@/asset/assets'
+
+const Contact = () => {
+
+ const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "11e77fec-28b3-447d-9178-c056c70547a4");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+
+	return (
+		<div
+			id="contact"
+			className="w-full px-[12%] py-10 scroll-mt-20 bg-no-repeat bg-center"
+			style={{
+				backgroundImage: "url('/footer-bg-color.png')",
+				backgroundSize: '90% auto',
+			}}
+		>
+			<h4 className="text-center mb-2 text-lg font-ovo">
+				My portfolio
+			</h4>
+
+			<h2 className="text-center text-5xl font-ovo">
+				My Latest Work
+			</h2>
+
+			<p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+				Have a new idea, feedback, or something exciting to share?
+				I’d love to hear it. Let’s connect and create something great together.
+			</p>
+
+			<form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
+				<div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
+					<input type="text" placeholder='Enter your name' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='name'/>
+					<input type="email" placeholder='Enter your email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='email'/>
+				</div>
+				<textarea rows='6' placeholder='Enter your message' required className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' name='message'></textarea>
+
+				<button
+					type="submit"
+					className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500">Submit now
+					<Image src={assets.right_arrow_white} alt="" className="w-4"/>
+				</button>
+
+				<p className='mt-4'>{result}</p>
+			</form>
+		</div>
+	)
+}
+
+export default Contact
