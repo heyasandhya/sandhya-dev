0cbf184adc27ebc8721a995399fc807acb8f8ba9
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-08-27 22:32:24 +0530

    add dark mode functionality and update global styles

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client'
+import { useState, useEffect } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
@@ -8,15 +9,37 @@ import Work from "./components/Work";
 import Footer from "./components/Footer";
 
 export default function Home() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    }
+    else {
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = '';
+    }
+  }, [isDarkMode]);
+
   return (
     <>
-    <Navbar />
-    <Header />
-    <About />
-    <Services />
-    <Work />
-    <Contact />
-    <Footer />
+      <Navbar isDarkMode = {isDarkMode} setIsDarkMode = {setIsDarkMode} />
+      <Header isDarkMode = {isDarkMode}/>
+      <About isDarkMode = {isDarkMode}/>
+      <Services isDarkMode = {isDarkMode}/>
+      <Work isDarkMode = {isDarkMode}/>
+      <Contact isDarkMode = {isDarkMode}/>
+      <Footer isDarkMode = {isDarkMode}/>
     </>
   );
 }
