e7b73a62a5256423d9ae309d4b11c37f38316dcd
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-08-30 13:47:38 +0530

    add dark mode support to Contact and Footer

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,73 +1,109 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
 import { assets } from '@/asset/assets'
+import { motion } from 'motion/react'
 
-const Contact = () => {
+const Contact = ({ isDarkMode }) => {
 
- const [result, setResult] = useState("");
+	const [result, setResult] = useState("");
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    setResult("Sending....");
-    const formData = new FormData(event.target);
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		setResult("Sending....");
+		const formData = new FormData(event.target);
 
-    formData.append("access_key", "11e77fec-28b3-447d-9178-c056c70547a4");
+		formData.append("access_key", "11e77fec-28b3-447d-9178-c056c70547a4");
 
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData
-    });
+		const response = await fetch("https://api.web3forms.com/submit", {
+			method: "POST",
+			body: formData
+		});
 
-    const data = await response.json();
+		const data = await response.json();
 
-    if (data.success) {
-      setResult("Form Submitted Successfully");
-      event.target.reset();
-    } else {
-      console.log("Error", data);
-      setResult(data.message);
-    }
-  };
+		if (data.success) {
+			setResult("Form Submitted Successfully");
+			event.target.reset();
+		} else {
+			console.log("Error", data);
+			setResult(data.message);
+		}
+	};
 
 
 	return (
-		<div
+		<motion.div
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			transition={{ delay: 1 }}
 			id="contact"
-			className="w-full px-[12%] py-10 scroll-mt-20 bg-no-repeat bg-center"
+			className="w-full px-[12%] py-10 scroll-mt-20 bg-no-repeat bg-center dark:bg-none"
 			style={{
-				backgroundImage: "url('/footer-bg-color.png')",
+				backgroundImage: isDarkMode
+					? 'none'
+					: "url('/footer-bg-color.png')",
 				backgroundSize: '90% auto',
 			}}
 		>
-			<h4 className="text-center mb-2 text-lg font-ovo">
+			<motion.h4
+				initial={{ y: -20, opacity: 0 }}
+				whileInView={{ y: 0, opacity: 1 }}
+				transition={{ delay: 0.5, duration: 0.5 }}
+				className="text-center mb-2 text-lg font-ovo">
 				My portfolio
-			</h4>
+			</motion.h4>
 
-			<h2 className="text-center text-5xl font-ovo">
+			<motion.h2
+				initial={{ y: -20, opacity: 0 }}
+				whileInView={{ y: 0, opacity: 1 }}
+				transition={{ delay: 0.5, duration: 0.5 }}
+				className="text-center text-5xl font-ovo">
 				My Latest Work
-			</h2>
+			</motion.h2>
 
-			<p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+			<motion.p
+				initial={{ opacity: 0 }}
+				whileInView={{ opacity: 1 }}
+				transition={{ delay: 0.7, duration: 0.5 }}
+				className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
 				Have a new idea, feedback, or something exciting to share?
 				I’d love to hear it. Let’s connect and create something great together.
-			</p>
+			</motion.p>
 
-			<form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
+			<motion.form
+				initial={{ opacity: 0 }}
+				whileInView={{ opacity: 1 }}
+				transition={{ delay: 0.9, duration: 0.5 }}
+				onSubmit={onSubmit} className='max-w-2xl mx-auto'>
 				<div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
-					<input type="text" placeholder='Enter your name' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='name'/>
-					<input type="email" placeholder='Enter your email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='email'/>
-				</div>
-				<textarea rows='6' placeholder='Enter your message' required className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' name='message'></textarea>
+					<motion.input
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1 }}
+						transition={{ delay: 1.1, duration: 0.6 }}
+						type="text" placeholder='Enter your name' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='name' />
 
-				<button
+					<motion.input
+						initial={{ x: 50, opacity: 0 }}
+						whileInView={{ x: 0, opacity: 1 }}
+						transition={{ delay: 1.2, duration: 0.6 }} type="email" placeholder='Enter your email' required className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='email' />
+				</div>
+				<motion.textarea
+					initial={{ y: 100, opacity: 0 }}
+					whileInView={{ y: 0, opacity: 1 }}
+					transition={{ delay: 1.3, duration: 0.6 }}
+					rows='6' placeholder='Enter your message' required className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90' name='message'></motion.textarea>
+
+				<motion.button
+				whileHover={{scale:1.05}}
+				transition={{duration:0.3}}
 					type="submit"
-					className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500">Submit now
-					<Image src={assets.right_arrow_white} alt="" className="w-4"/>
-				</button>
+					className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] data:hover:bg-darkHover">Submit now
+					<Image src={assets.right_arrow_white} alt="" className="w-4" />
+				</motion.button>
 
 				<p className='mt-4'>{result}</p>
-			</form>
-		</div>
+			</motion.form>
+		</motion.div>
 	)
 }
 
