48af7bff84a1844b252e5c2c37dd2e77bca7d1c6
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-09-11 15:07:26 +0530

    refactor Footer component

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,28 +2,29 @@ import React from 'react'
 import Image from 'next/image'
 import { assets } from '@/asset/assets'
 
-const Footer = ({isDarkMode}) => {
-  return (
-	<div className='mt-20'>
-	  <div className='text-center'>
-		<Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+const Footer = ({ isDarkMode }) => {
+	return (
+		<div className='mt-20'>
+			<div className='text-center'>
+				<a href="#top" className='text-3xl font-ovo font-bold'>
+					Sandhya<span className='text-pink-500'>.</span>
+				</a>
+				<div className='w-max flex items-center gap-2 mx-auto'>
+					<Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6' />
+					sandhyasingh950823256@gmail.com
+				</div>
+			</div>
 
-		<div className='w-max flex items-center gap-2 mx-auto'>
-			<Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
-				sandhyasingh950823256@gmail.com
+			<div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+				<p>© 2026 Sandhya Kumari. All rights reserved.</p>
+				<ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
+					<li><a target='_blank' href="https://github.com/heyasandhya">GitHub</a></li>
+					<li><a target='_blank' href="https://www.linkedin.com/in/sandhyakumari2/">Linkedin</a></li>
+				</ul>
+			</div>
+
 		</div>
-	  </div>
-
-		<div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-			<p>© 2026 Sandhya Kumari. All rights reserved.</p>
-			<ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-				<li><a target='_blank' href="https://github.com/heyasandhya">GitHub</a></li>
-				<li><a target='_blank' href="https://www.linkedin.com/in/sandhyakumari2/">Linkedin</a></li>
-			</ul>
-		</div>
-
-	</div>
-  )
+	)
 }
 
 export default Footer
